9a829683dace0e9cc2cb237986fe8d232eb06eae
Protodile <devin.o.riley@gmail.com>
2016-05-10 19:01:58 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ function respond() {
       botRegex = /^\/cool guy/;  botRegexDL = /^\/DDL/i;botRegexSalt = /^\/salt/;botRegexRules = /^\/rules/
       botRegexAd=/^\/advance/;botRegexPB = /^\/pablo/; botRegexSC = /^\/SDL/i; botODB = /(.*\s+)(.*odb)(\s+.*)/i; botDuck = /^\/duck/;
       botRegexP = /^\/PDL/i;  botRegexTw = /^\/twitch/i; botRegexSb = /^\/sub/; botRegexSh = /^\/shrug/; botRegexWk = /^\/users/; botRegexCC = /^\/cc/;
-      botRegexSiege = /^\/siege/; botRegexOW = /^\/overwatch/; 
+      botRegexSiege = /^\/siege/; botRegexAss = /^\/ass/; 
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
   var teamAb = ["NE","NO","ARI","PHI","CLE","TEN","OAK","DAL","IND","SEA","CIN","PIT","JAC"
@@ -25,9 +25,9 @@ function respond() {
     this.res.end();
   } 
   
-  else if(request.text && botRegexOW.test(request.text)) {
+  else if(request.text && botRegexAss.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("http://kt-media.knowtechie.netdna-cdn.com/wp-content/uploads/2016/03/Overwatch-Tracer-Over-The-Shoulder.png");
+    postMessage("https://s-media-cache-ak0.pinimg.com/736x/bf/de/aa/bfdeaa758de10f072925051a0770ed1c.jpg");
     this.res.end();
   } 
   else if(request.text && botRegexSalt.test(request.text)) {
